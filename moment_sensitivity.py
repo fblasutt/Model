7ec63b12425791f144c3c158cd9bfbe3fabb2ba6
sara-casella <scasella@luiss.it>
2025-10-23 19:01:29 +0200

    update moment_sensitivity

--- moment_sensitivity.py
+++ moment_sensitivity.py
@@ -11,10 +11,10 @@ but this is not done here
 @author: sara
 """
 # %% 
-import os
-os.environ.setdefault("OMP_NUM_THREADS", "1")
-os.environ.setdefault("MKL_NUM_THREADS", "1")
-os.environ.setdefault("NUMEXPR_MAX_THREADS", "1")
+# import os
+# os.environ.setdefault("OMP_NUM_THREADS", "1")
+# os.environ.setdefault("MKL_NUM_THREADS", "1")
+# os.environ.setdefault("NUMEXPR_MAX_THREADS", "1")
 
 # %%
 
